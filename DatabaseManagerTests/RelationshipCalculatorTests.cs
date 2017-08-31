7639246c249080a11b4be1de811bbe0ffd523421
MorganAbraham <mabraham1986@yahoo.com>
2017-08-30 22:36:05 -0400

    Added matrix method to determine familial relationships

---
+++ DatabaseManagerTests/RelationshipCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Deacon_Database_Manager.MemberData;
+namespace DatabaseManagerTests
+{
+    [TestClass]
+    public class RelationshipCalculatorTests
+    {
+        [TestMethod]
+        public void GetRelationship_Grandmother()
+        {
+            string RelationToMember = "Mother";
+            string RelationToRelation = "Mother";
+            string ExpResult = "Grandmother";
+            RelationshipCalculator RCalc = new RelationshipCalculator();
+            string Result = RCalc.GetRelationship(RelationToMember, RelationToRelation);
+            Assert.AreEqual(ExpResult, Result);
+        }
+
+
+        [TestMethod]
+        public void GetRelationship_Wife()
+        {
+            string RelationToMember = "Grandson";
+            string RelationToRelation = "Grandmother";
+            string ExpResult = "Wife";
+            RelationshipCalculator RCalc = new RelationshipCalculator();
+            string Result = RCalc.GetRelationship(RelationToMember, RelationToRelation);
+            Assert.AreEqual(ExpResult, Result);
+
+        }
+        [TestMethod]
+        public void GetRelationship_Null()
+        {
+            string RelationToMember = "Grandson";
+            string RelationToRelation = "Mother-In-Law";
+            string ExpResult = null;
+            RelationshipCalculator RCalc = new RelationshipCalculator();
+            string Result = RCalc.GetRelationship(RelationToMember, RelationToRelation);
+            Assert.AreEqual(ExpResult, Result);
+
+        }
+    }
+}
--- Deacon Database Manager/MemberData/RelationshipCalculator.cs
+++ Deacon Database Manager/MemberData/RelationshipCalculator.cs
@@ -16,5 +16,68 @@ namespace Deacon_Database_Manager.MemberData
             //List<Member> Relatives = GetAllRelationships()
             return Result;
         }
+
+        public string GetRelationship(string RelationToMember, string RelationToRelation)
+        {
+            string[,] Matrix = new string[,]
+            {
+                {"","Mother","Father","Brother","Sister","Grandmother","Grandfather","Grandson","Granddaughter","Great-Grandmother","Great-Grandfather","Father-In-Law","Mother-In-Law","Sister-In-Law","Brother-In-Law","Son-In-Law","Daughter-In-Law","Husband","Wife","Aunt","Uncle"},
+                {"Mother","Grandmother","Grandfather","Uncle","Aunt","Great-Grandmother","Great-Grandfather","Nephew/Son","Niece/Daughter","Great-Great Grandmother","Great-Great Grandfather","Grandfather","Grandmother","Aunt","Uncle","Husband/Brother-In-Law","Wife/Sister-In-Law","Father","Mother","-","-"},
+                {"Father","Grandmother","Grandfather","Uncle","Aunt","Great-Grandmother","Great-Grandfather","Nephew/Son","Niece/Daughter","Great-Great Grandmother","Great-Great Grandfather","-","-","-","-","-","-","Father","Mother","-","-"},
+                {"Brother","-","-","-","-","-","-","Great-Nephew","Great-Niece","-","-","-","-","-","-","-","-","Brother-In-Law","Sister-In-Law","-","-"},
+                {"Sister","-","-","-","-","-","-","Great-Nephew","Great-Niece","-","-","-","-","-","-","-","-","Brother-In-Law","Sister-In-Law","-","-"},
+                {"Grandmother","Great-Grandmother","Great-Grandfather","Great-Uncle","Great-Aunt","Great-Great Grandmother","Great-Great Grandfather","Cousin","Cousin","-","-","-","-","-","-","-","-","Grandfather","Grandmother","-","-"},
+                {"Grandfather","Great-Grandmother","Great-Grandfather","Great-Uncle","Great-Aunt","Great-Great Grandmother","Great-Great Grandfather","Cousin","Cousin","-","-","-","-","-","-","-","-","Grandfather","Grandmother","-","-"},
+                {"Grandson","Daughter/Daughter-In-Law","Son/Son-In-Law","-","-","Wife","Husband","Great-Great Grandson","Great-Great Granddaughter","-","-","-","-","-","-","-","-","-","-","-","-"},
+                {"Granddaugther","Daughter/Daughter-In-Law","Son/Son-In-Law","-","-","Wife","Husband","Great-Great Grandson","Great-Great Granddaughter","-","-","-","-","","","","","-","-","-","-"},
+                {"Great-Grandmother","Great-Great Grandmother","Great-Great Grandfather","Great-Great Uncle","Great-Great-Aunt","Great-Great-Grandmother","Great-Great Grandfather","Father/Uncle","Mother/Aunt","-","-","-","-","-","-","-","-","Great-Grandfather","Great-Grandmother","-","-"},
+                {"Great-Grandfather","Great-Great Grandmother","Great-Great Grandfather","Great-Great Uncle","Great-Great-Aunt","Great-Great-Grandfather","Great-Great Grandfather","Father/Uncle","Mother/Aunt","-","-","-","-","-","-","-","-","Great-Grandfather","Great-Grandmother","-","-"},
+                {"Father-In-Law","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","Father-In-Law","Mother-In-Law","-","-"},
+                {"Mother-In-Law","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","Father-In-Law","Mother-In-Law","-","-"},
+                {"Sister-In-Law","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","Brother-In-Law","Sister-In-Law","-","-"},
+                {"Brother-In-Law","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","Brother-In-Law","Sister-In-Law","-","-"},
+                {"Son-In-Law","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","Son","Daughter","-","-"},
+                {"Daughter-In-Law","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","Son","Daughter","-","-"},
+                {"Husband","Mother-In-Law","Father-In-Law","Brother-In-Law","Sister-In-Law","-","-","Grandson","Granddaughter","-","-","Father","Mother","Sister","Brother","Son-In-Law","Daughter-In-Law","-","-","-","-"},
+                {"Wife","Mother-In-Law","Father-In-Law","Brother-In-Law","Sister-In-Law","-","-","Grandson","Granddaughter","-","-","Father","Mother","Sister","Brother","Son-In-Law","Daughter-In-Law","-","-","-","-"},
+                {"Aunt","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","Uncle","Aunt","-","-"},
+                {"Uncle","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","Uncle","Aunt","-","-"},
+
+            };
+            string Result = null;
+
+            int RowIndex = -1;
+            int ColumnIndex = -1;
+
+            for(int Row = 0; Row < Matrix.GetLength(0); Row++)
+            {
+                if(Matrix[Row, 0] == RelationToMember)
+                {
+                    RowIndex = Row;
+                    break;
+                }
+            }
+
+            for(int Column = 0; Column < Matrix.GetLength(1); Column++)
+            {
+                if(Matrix[0, Column] == RelationToRelation)
+                {
+                    ColumnIndex = Column;
+                    break;
+                }
+            }
+
+            if(RowIndex > -1 && ColumnIndex > -1)
+            {
+                Result = Matrix[RowIndex, ColumnIndex];
+            }
+
+            if(Result == "-" || Result.IndexOf(@"/") > -1)
+            {
+                Result = null;
+            }
+            return Result;
+        }
+            
     }
 }
